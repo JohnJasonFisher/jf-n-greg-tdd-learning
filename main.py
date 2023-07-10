5f8d6452afee300f1d0c035264afdf7692a21d76
Gregory Decuir <decuir1990@hotmail.com>
2023-07-09 22:20:49 -0500

    add dimes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 input_number = 12
+#input_number = 7
+#input_number = 62
 
 pennies = 0
 nickel = 0
-# dime = 0
+dime = 0
+
+while (input_number > 10):
+    dime +=1
+    input_number  -= 10
 
 while (input_number > 5):
     nickel +=1
@@ -10,4 +16,4 @@ while (input_number > 5):
 
 pennies = input_number
 
-print(str(nickel) + ' nickel, ' + str(pennies) + ' pennies')
+print(str(dime) + ' dimes, ' + str(nickel) + ' nickels, ' + str(pennies) + ' pennies')

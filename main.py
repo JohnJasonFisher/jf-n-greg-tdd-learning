dc72b41933dd0ca4841b121868c59ff7bb00a87c
JohnJasonFisher <jjfisher91@gmail.com>
2023-07-09 22:30:40 -0500

    Adds test file

--- main.py
+++ main.py
@@ -15,12 +15,8 @@ def return_big_coins(input_number):
 
     return (str(dime) + ' dimes, ' + str(nickel) + ' nickels, ' + str(pennies) + ' pennies')
 
-if (return_big_coins(7) == '0 dimes, 1 nickels, 2 pennies'):
-    print(return_big_coins(7) + ' is correct')
-else:
-    print(return_big_coins(7) + ' is incorrect')
-
-# print(return_big_coins(12))
-# print(return_big_coins(27))
-# print(return_big_coins(62))
-
+if (__name__ == '__main__'):
+    print(return_big_coins(7))
+    print(return_big_coins(12))
+    print(return_big_coins(27))
+    print(return_big_coins(62))
---
+++ main_test.py
@@ -0,0 +1,6 @@
+from main import return_big_coins
+
+if (return_big_coins(7) == '0 dimes, 1 nickels, 2 pennies'):
+    print(return_big_coins(7) + ' is correct')
+else:
+    print(return_big_coins(7) + ' is incorrect')

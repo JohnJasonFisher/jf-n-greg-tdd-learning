a55d23a93343b29f0a67f2e017f3417ce097fb47
Gregory Decuir <decuir1990@hotmail.com>
2023-07-09 22:13:16 -0500

    solve for 12

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 input_number = 12
 
-print(str(input_number) + ' pennies')
+pennies = 0
+nickel = 0
+# dime = 0
+
+while (input_number > 5):
+    nickel +=1
+    input_number  -= 5
+
+pennies = input_number
+
+print(str(nickel) + ' nickel, ' + str(pennies) + ' pennies')

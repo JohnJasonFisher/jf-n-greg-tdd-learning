b25f345567294c44acac28228d707323ae98a82c
JohnJasonFisher <jjfisher91@gmail.com>
2023-07-09 22:44:35 -0500

    Simplifies print message

--- main_test.py
+++ main_test.py
@@ -4,14 +4,14 @@ expected_message = '1 nickels, 2 pennies'
 actual_message = return_big_coins(7)
 
 if (actual_message == expected_message):
-    print(actual_message + ' is correct')
+    print('Is correct')
 else:
-    print(actual_message + ' is incorrect')
+    print('Is incorrect')
 
 expected_message = '1 dimes, 2 pennies'
 actual_message = return_big_coins(12)
 
 if (actual_message == expected_message):
-    print(actual_message + ' is correct')
+    print('Is correct')
 else:
-    print(actual_message + ' is incorrect')
+    print('Is incorrect')
